5dd415b3131f52199f0b72505132bb5c7405a1c6
Quentin <quentin.chateau@gmail.com>
2016-10-12 14:38:15 +0200

    Use BORDER_REPLICATE for gain map filtering

--- modules/stitching/src/exposure_compensate.cpp
+++ modules/stitching/src/exposure_compensate.cpp
@@ -309,8 +309,8 @@ void BlocksGainCompensator::feed(const std::vector<Point> &corners, InputArrayOf
                 }
             }
         }
-        sepFilter2D(gain_maps_[img_idx], gain_maps_[img_idx], CV_32F, ker, ker);
-        sepFilter2D(gain_maps_[img_idx], gain_maps_[img_idx], CV_32F, ker, ker);
+        sepFilter2D(gain_maps_[img_idx], gain_maps_[img_idx], CV_32F, ker, ker, Point(-1,-1), 0, BORDER_REPLICATE);
+        sepFilter2D(gain_maps_[img_idx], gain_maps_[img_idx], CV_32F, ker, ker, Point(-1,-1), 0, BORDER_REPLICATE);
     }
 }
 
